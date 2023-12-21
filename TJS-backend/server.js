23bab57d8e9369f174b2e479eb01f6ffd9eaac1d
Titus Kovalik <titus.kovalik@tlgcohort.com>
2023-12-21 10:32:20 -0800

    prepared the backend to recieve breakfast lunch and dinner

--- TJS-backend/server.js
+++ TJS-backend/server.js
@@ -20,14 +20,42 @@ app.use(express.json())
 app.use(express.urlencoded({ extended : false}))
 app.use(cors())
 
-app.get('/addfavorites', function (req, res) {
-    res.send(favorites)
-})
 
 app.post('/addfavorites', function (req, res) {
     favorites.push(req.body)
     res.redirect('/addfavorites')
 })
+app.get('/addfavorites', function (req, res) {
+    res.send(favorites)
+})
+
+
+app.post('/addbreakfast', function (req, res) {
+    breakfast.push(req.body)
+    res.redirect('/addbreakfast')
+})
+app.get('/addbreakfast', function (req, res) {
+    res.send(favorites)
+})
+
+
+app.post('/addlunch', function (req, res) {
+    lunch.push(req.body)
+    res.redirect('/addlunch')
+})
+app.get('/addlunch', function (req, res) {
+    res.send(favorites)
+})
+
+
+app.post('/adddinner', function (req, res) {
+    dinner.push(req.body)
+    res.redirect('/adddinner')
+})
+app.get('/adddinner', function (req, res) {
+    res.send(favorites)
+})
+
 
 app.listen(PORT, async () => {
     console.log(`Now listening on port: ${PORT}`);
